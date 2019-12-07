aa49570f73c6f7b8e27d0e8b6761c260a63804d0
Benjamin Strauch <b.strauch@fu-berlin.de>
2019-12-07 22:46:02 +0100

    Day 5 finally finished

--- day5/day5.clj
+++ day5/day5.clj
@@ -14,6 +14,11 @@
     (mod (quot n shift) 10)))
 
 (defn parse-instruction [mem]
+  "Parse the ABCDE instruction at the start of this mem region.
+  
+  ABC contain the parameter modes for parameters 3/2/1 and DE
+  stores the actual opcode. We return the function that this
+  opcode computes and data about its arguments and param modes."
  (let [instruction (first mem)
        opcode (mod instruction 100)
        pmodes (mapv #(get-digit instruction %) (range 2 5))]
@@ -21,15 +26,25 @@
      1 {:f + :in-args 2 :out-pos 2 :pmodes pmodes}
      2 {:f * :in-args 2 :out-pos 2 :pmodes pmodes}
      3 {:f #(Integer/parseInt (read-line)) :in-args 0 :out-pos 0 :pmodes pmodes}
-     4 {:f #(println "RESULT:" %) :in-args 1 :out-pos nil :pmodes pmodes}
+     4 {:f #(println "CODE:" %) :in-args 1 :out-pos nil :pmodes pmodes}
+     5 {:f #(if (zero? %1) nil %2) :in-args 2 :out-pos nil :pmodes pmodes}
+     6 {:f #(if (zero? %1) %2 nil) :in-args 2 :out-pos nil :pmodes pmodes}
+     7 {:f #(if (< %1 %2) 1 0) :in-args 2 :out-pos 2 :pmodes pmodes}
+     8 {:f #(if (= %1 %2) 1 0) :in-args 2 :out-pos 2 :pmodes pmodes}
      99 :halt
    )))
 
-(defn ip-offset [opcode] 
-  "Determine how far to advance the instruction pointer after this opcode.
-  - for instructions with output, go to the pos after the output.
-  - for instructions without output, go to the pos after the last input."
-  (if-let [out-pos (:out-pos opcode)] (+ out-pos 2) (inc (:in-args opcode))))
+(defn get-next-ip-addr [ip opcode result] 
+  "Get the next IP after the opcode has run and returned the result.
+  - for instructions with output, simply go to the pos after the output.
+  - for instructions without output (print or jumps), there are two options:
+      - if they return `nil`, the IP just goes past their params
+      - if they return a value, it's interpreted as an address and the IP set to it"
+  (if-let [out-pos (:out-pos opcode)] 
+    (+ ip out-pos 2) 
+    (if-let [target-addr result] 
+      target-addr
+      (+ ip (inc (:in-args opcode))))))
 
 (defn addr-getter [mem ip pmodes]
   "Return a function that loads the address at ip+offset. Depending
@@ -50,11 +65,14 @@
       (let [in-addrs (mapv load-addr (range (:in-args opcode)))
             in-vals (mapv #(nth mem %) in-addrs)
             result (apply (:f opcode) in-vals)
-            next-ip (+ ip (ip-offset opcode))]
+            next-ip (get-next-ip-addr ip opcode result)]
         (if (some? (:out-pos opcode))
           (eval-program (assoc mem (load-addr (:out-pos opcode)) result) next-ip)
           (eval-program mem next-ip)))))))
 
+; For testing - should output the value we input.
 (def test-program (vector 3 0 4 0 99))
 (eval-program test-program)
-;(eval-program problem-input)
+
+; The actual program - input 1 for part 1 and input 5 for part 2.
+(eval-program problem-input)
